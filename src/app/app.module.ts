43a4302dca9be47eb2126918c2539229c792c232
halilbahar <halil.s.bahar@gmail.com>
2021-05-09 01:45:07 +0200

    Display canvas and sidebar side by side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanvasComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
