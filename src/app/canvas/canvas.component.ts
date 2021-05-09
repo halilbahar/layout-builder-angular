5adc6e72f76bb6808ea0299bfbf4e5386876e9f1
halilbahar <halil.s.bahar@gmail.com>
2021-05-09 02:01:01 +0200

    Create fabric canvas

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-canvas',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CanvasComponent implements OnInit {
 
+  @ViewChild('screenWrapper', { static: true }) screenWrapper: ElementRef;
+  @ViewChild('screen', { static: true }) screen: ElementRef;
+  canvas: fabric.Canvas;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.canvas = new fabric.Canvas(this.screen.nativeElement);
+    this.canvas.setWidth(1920);
+    this.canvas.setHeight(1080);
+    this.resize();
   }
 
+  @HostListener('window:resize') onResize(): void {
+    this.resize();
+  }
+
+  private resize(): void {
+    const element = this.screenWrapper.nativeElement as HTMLElement;
+    const parent = element.parentElement;
+
+    const scale = Math.min(
+      (parent.clientWidth * 0.85) / element.clientWidth,
+      (parent.clientHeight * 0.85) / element.clientHeight
+    );
+
+    element.style.transform = `translate(-50%, -50%) scale(${scale})`;
+  }
 }
